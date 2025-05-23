fdd03a3d39f5f0a96019e6267a452ee5d6f0b316
Hyunah Lee <hyuna030619@naver.com>
2025-05-23 18:44:14 +0900

    initial commit

---
+++ src/main/java/swe4/saju_taro/SajuTaroApplication.java
@@ -0,0 +1,13 @@
+package swe4.saju_taro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajuTaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajuTaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swe4/saju_taro/SajuTaroApplicationTests.java
@@ -0,0 +1,13 @@
+package swe4.saju_taro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajuTaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
