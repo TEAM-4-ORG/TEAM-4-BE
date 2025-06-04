5bc8c2820c785c83f245506b232ef44188825029
Jeongho Kim <kimjungho.kr@gmail.com>
2025-06-04 23:44:35 +0900

    feat: 유저 생성api에서 response에 user_id 추가

--- src/main/java/swe4/saju_taro/controller/UserController.java
+++ src/main/java/swe4/saju_taro/controller/UserController.java
@@ -8,6 +8,9 @@ import swe4.saju_taro.dto.CommonResponse;
 import swe4.saju_taro.dto.UserRequest;
 import swe4.saju_taro.service.UserService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -27,7 +30,7 @@ public class UserController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<CommonResponse> newUser(@RequestBody UserRequest request){
+    public ResponseEntity<CommonResponse<Map<String, Object>>> newUser(@RequestBody UserRequest request){
         User user = new User();
         user.setBirth(request.getBirth());
         user.setTime(request.getTime());
@@ -36,36 +39,38 @@ public class UserController {
         boolean created = userService.createUser(user);
 
         if (created) {
-            return ResponseEntity.ok(new CommonResponse(true, "COMMON200", "유저 추가에 성공했습니다."));
+            Map<String, Object> result = new HashMap<>();
+            result.put("user_id", user.getUserId());
+
+            return ResponseEntity.ok(new CommonResponse<>(true, "COMMON200", "유저 추가에 성공했습니다.", result));
         } else {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new CommonResponse(false, "USER500", "유저 추가에 실패했습니다."));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new CommonResponse<>(false, "USER500", "유저 추가에 실패했습니다."));
         }
     }
 
     @PutMapping("/change/{userId}")
-    public ResponseEntity<CommonResponse> changeUserInfo(@PathVariable("userId") Integer userId, @RequestBody UserRequest request){
+    public ResponseEntity<CommonResponse<Void>> changeUserInfo(@PathVariable("userId") Integer userId, @RequestBody UserRequest request){
         boolean updated = userService.updateUser(userId, request);
 
         if (updated) {
             return ResponseEntity.ok(
-                    new CommonResponse(true, "COMMON200", "유저 정보 수정에 성공했습니다."));
+                    new CommonResponse<>(true, "COMMON200", "유저 정보 수정에 성공했습니다."));
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new CommonResponse(false, "USER404", "해당 유저를 찾을 수 없습니다."));
+                    .body(new CommonResponse<>(false, "USER404", "해당 유저를 찾을 수 없습니다."));
         }
     }
 
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<CommonResponse> deleteUser(@PathVariable("userId") Integer userId) {
+    public ResponseEntity<CommonResponse<Void>> deleteUser(@PathVariable("userId") Integer userId) {
         boolean deleted = userService.deleteUser(userId);   //userService 작성해야함
         if (deleted) {
-            return ResponseEntity.ok(new CommonResponse(true, "COMMON200", "유저 삭제에 성공했습니다."));
+            return ResponseEntity.ok(new CommonResponse<>(true, "COMMON200", "유저 삭제에 성공했습니다."));
         } else {
             return ResponseEntity
                     .status(HttpStatus.NOT_FOUND)
-                    .body(new CommonResponse(false, "COMMON404", "해당 유저를 찾을 수 없어 삭제에 실패했습니다."));
+                    .body(new CommonResponse<>(false, "COMMON404", "해당 유저를 찾을 수 없어 삭제에 실패했습니다."));
         }
     }
 //    @GetMapping("/list/{userId}/projects")
--- src/main/java/swe4/saju_taro/dto/CommonResponse.java
+++ src/main/java/swe4/saju_taro/dto/CommonResponse.java
@@ -2,6 +2,7 @@ package swe4.saju_taro.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
@@ -11,8 +12,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonPropertyOrder({ "isSuccess", "code", "message" })
-public class CommonResponse {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "isSuccess", "code", "message", "result" })
+public class CommonResponse<T> {
 
     @JsonProperty("isSuccess")
     private boolean isSuccess;
@@ -23,9 +25,19 @@ public class CommonResponse {
     @JsonProperty("message")
     private String message;
 
+    @JsonProperty("result")
+    private T result;
+
     @JsonIgnore
     public boolean getSuccess() {
         return isSuccess;
+    }
+
+    public CommonResponse(boolean isSuccess, String code, String message, T result) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+        this.result = result;
     }
 
     public CommonResponse(boolean isSuccess, String code, String message) {
@@ -44,5 +56,9 @@ public class CommonResponse {
 
     public String getMessage() {
         return message;
+    }
+
+    public T getResult(){
+        return result;
     }
 }
