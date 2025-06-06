1683acf0e11a8b1393bca8b7c4e91afbfba6e7f2
Hyunah Lee <hyuna030619@naver.com>
2025-06-07 01:38:00 +0900

    chore: JsonProperty 추가

--- src/main/java/swe4/saju_taro/dto/ProjectRequestDTO.java
+++ src/main/java/swe4/saju_taro/dto/ProjectRequestDTO.java
@@ -1,5 +1,6 @@
 package swe4.saju_taro.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,10 @@ public class ProjectRequestDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ProjectDTO {
+        @JsonProperty("user_id")
         private Integer userId;
         private Type type;
+        @JsonProperty("first_question")
         private String firstQuestion;
     }
 }
