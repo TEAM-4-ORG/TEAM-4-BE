239aaa23c8e89569c3d9bc8ad5a95bcc586ab97a
Hyunah Lee <hyuna030619@naver.com>
2025-06-06 19:46:57 +0900

    feat: JsonProperty 수정

--- src/main/java/swe4/saju_taro/dto/ProjectResponseDTO.java
+++ src/main/java/swe4/saju_taro/dto/ProjectResponseDTO.java
@@ -1,5 +1,6 @@
 package swe4.saju_taro.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -16,7 +17,9 @@ public class ProjectResponseDTO {
     @Getter
     @AllArgsConstructor
     public static class ProjectLoadDTO {
+        @JsonProperty("project_id")
         private Integer projectId;
+        @JsonProperty("user_id")
         private Integer userId;
         private String title;
         private String type;
