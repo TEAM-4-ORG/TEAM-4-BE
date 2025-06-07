3e78145c3998ece8012991c628428e0ca90da85e
Jeongho Kim <kimjungho.kr@gmail.com>
2025-06-07 23:12:27 +0900

    fix: 유저 정보 조회 project 무한하게 나오는 오류 수정

--- src/main/java/swe4/saju_taro/dto/UserResponseDTO.java
+++ src/main/java/swe4/saju_taro/dto/UserResponseDTO.java
@@ -2,6 +2,14 @@ package swe4.saju_taro.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import swe4.saju_taro.domain.Project;
+import swe4.saju_taro.domain.User;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserResponseDTO {
 
@@ -9,7 +17,16 @@ public class UserResponseDTO {
     @AllArgsConstructor
     public static class UserDTO{
         private Integer user_id;
+    }
 
+    @Getter
+    @AllArgsConstructor
+    public static class UserLoadDTO{
+        private Integer user_id;
+        private LocalDate birth;
+        private LocalTime time;
+        private boolean gender;
+        private List<ProjectResponseDTO.ProjectUserLoadDTO> projects;
     }
 
 }
